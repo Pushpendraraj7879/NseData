d9809b0c74814e0e680b84a02d0aa8501e588cda
pushpendraraj7879 <pushpendraraj7879@gmail.com>
2025-01-03 15:23:43 +0530

    next

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
-const express=require("express")
-const { nseData } =require('nse-data');
-const app=express()
-const PORT=process.env.PORT || 3000
-const data=[]
-app.get('/nseData',(req,res)=>{
-    nseData.marketStatus().then((value) => {
-        data=value
-    })
-    res.status(200).json(value)
-})
+const express = require("express");
+const { nseData } = require("nse-data");
+const app = express();
+const PORT = process.env.PORT || 3000;
 
+app.get("/nseData", (req, res) => {
+  nseData.marketStatus().then((value) => { res.status(200).json(value);}).catch((error)=>{ res.status(500).json(error)});
+});
 
-
-
-
-app.listen(PORT,(req,res)=>{
-    console.log(`Sever is running on ${PORT}` )
-})
+app.listen(PORT, (req, res) => {
+  console.log(`Sever is running on ${PORT}`);
+});
