e4518b8e6ba7f6287ba90d77339e74d54b446089
pushpendraraj7879 <pushpendraraj7879@gmail.com>
2025-01-03 15:04:54 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express=require("express")
+import { nseData } from 'nse-data';
+const app=express()
+const PORT=process.env.PORT || 3000
+const data=[]
+app.get('/nseData',(req,res)=>{
+    nseData.marketStatus().then((value) => {
+        data=value
+    })
+    res.status(200).json(value)
+})
+
+
+
+
+
+app.listen(PORT,(req,res)=>{
+    console.log(`Sever is running on ${PORT}` )
+})
